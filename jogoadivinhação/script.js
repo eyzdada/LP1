1a1bf069b60cd766c3fb32f060d58601c3595a2a
viniK31 <125459372+TLean07@users.noreply.github.com>
2024-09-11 13:04:12 -0300

    jogo

---
+++ jogoadivinhação/script.js
@@ -0,0 +1,15 @@
+function gerarNumeroAleatorio(){
+    const numeroAleatorio = Math.floor(Math.random() * 11)
+return numeroAleatorio
+}
+
+function Jogar(){
+    const pegaNumeroAleatorio = gerarNumeroAleatorio()
+    console.log(pegaNumeroAleatorio)
+do{
+    const palpite =prompt("Adivinha o Número")
+}
+while()
+}
+
+Jogar()
